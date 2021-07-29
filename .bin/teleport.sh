0309c6d1e6045c5498d1e50a9c8f4a02f4f855fa
Taro Uraoka <turaoka@circlace.com>
2021-07-29 10:36:45 +0900

    teleportのエラーを無視

--- .bin/teleport.sh
+++ .bin/teleport.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/zsh
 function teleport() {
-    COMMAND='kill $(ps x | grep /Applications/teleport.app/Contents/MacOS/teleport | grep -v grep | awk '\''{print $1}'\'') && open -a teleport'
+    COMMAND='kill $(ps x | grep /Applications/teleport.app/Contents/MacOS/teleport | grep -v grep | awk '\''{print $1}'\'') > /dev/null 2>&1 ; open -a teleport'
     HOST=$1
     if [ -n "${HOST}" ]; then
         ssh $HOST ${COMMAND}
